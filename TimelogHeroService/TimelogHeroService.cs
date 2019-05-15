397a6220320ce6d93272083b7dad6f2815f0ff9d
Cyril Gibaud <cyril.gibaud@toonkit-studio.com>
2019-05-15 20:25:27 +0200

    Initial commit

---
+++ TimelogHeroService/TimelogHeroService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimelogHeroService
+{
+    public partial class TimelogHeroService : ServiceBase
+    {
+        public TimelogHeroService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
